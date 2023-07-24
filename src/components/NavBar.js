e622d4140160d07fc1b9a602897194c1f278c1b8
Tirsa St. Fort <lifelovelori@gmail.com>
2023-07-24 12:32:56 -0400

    Complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-function NavBar() {
-  const links = ["home", "about", "projects"];
+function NavBar({home, about, projects}) {
+  // const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{
+    <><a key="home" href="#home">home</a><a key="about" href="#about">about</a><a key="projects" href="#projects">projects</a></>
+    /* display an <a> tag for each link here */}</nav>;
 }
 
 export default NavBar;
